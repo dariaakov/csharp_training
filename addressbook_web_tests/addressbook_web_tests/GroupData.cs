ea19f3f07d5cf175e1f1b9401136e24cb88614a3
daria.kovalenko@inbox.ru <daria.kovalenko@inbox.ru>
2020-03-07 16:18:30 +0300

    Задание 3 (исправлено)

--- addressbook_web_tests/addressbook_web_tests/GroupData.cs
+++ addressbook_web_tests/addressbook_web_tests/GroupData.cs
@@ -20,8 +20,8 @@ namespace WebAddressbookTests
         public GroupData(string name, string header, string footer)
         {
             this.name = name;
-            this.name = header;
-            this.name = footer;
+            this.header = header;
+            this.footer = footer;
         }
 
         public string Name 
