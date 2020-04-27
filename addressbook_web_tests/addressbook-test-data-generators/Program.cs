e8d2dff3e8f4fa3ef4e059318d5ff85d71238af9
daria.kovalenko@inbox.ru <daria.kovalenko@inbox.ru>
2020-04-27 20:57:20 +0300

    Лекция 6.4

--- addressbook_web_tests/addressbook-test-data-generators/Program.cs
+++ addressbook_web_tests/addressbook-test-data-generators/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
+using Newtonsoft.Json;
 using WebAddressbookTests;
 
 namespace addressbook_test_data_generators
@@ -16,15 +17,15 @@ namespace addressbook_test_data_generators
         {
             int count = Convert.ToInt32(args[0]);
             StreamWriter writer = new StreamWriter(args[1]);
-            string format = args[3];
+            string format = args[2];
 
             List<GroupData> groups = new List<GroupData>();
             for (int i = 0; i < count; i++)
             {
                 groups.Add(new GroupData(TestBase.GenerateRandomString(10))
                 {
-                    Header = TestBase.GenerateRandomString(10),
-                    Footer = TestBase.GenerateRandomString(10)
+                    Header = TestBase.GenerateRandomString(100),
+                    Footer = TestBase.GenerateRandomString(100)
                 });
             }
             if (format == "csv")
@@ -34,6 +35,10 @@ namespace addressbook_test_data_generators
             else if (format == "xml")
             {
                 writeGroupsToXMLFile(groups, writer);
+            }
+            else if (format == "json")
+            {
+                writeGroupsToJsonFile(groups, writer);
             }
             else
             {
@@ -55,6 +60,12 @@ namespace addressbook_test_data_generators
         {
             new XmlSerializer(typeof(List<GroupData>)).Serialize(writer, groups);
 
+        }
+
+        static void writeGroupsToJsonFile(List<GroupData> groups, StreamWriter writer)
+        {
+            writer.Write(JsonConvert.SerializeObject(groups, Newtonsoft.Json.Formatting.Indented));
+        
         }
     }
 }
--- addressbook_web_tests/addressbook_web_tests/Tests/GroupCreationTests.cs
+++ addressbook_web_tests/addressbook_web_tests/Tests/GroupCreationTests.cs
@@ -5,6 +5,9 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using System.IO;
 using NUnit.Framework;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
 
 namespace WebAddressbookTests
 {
@@ -27,7 +30,21 @@ namespace WebAddressbookTests
             return groups;
         }
 
-        public static IEnumerable<GroupData> GroupDataFromFile()
+        public static IEnumerable<GroupData> GroupDataFromXmlFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            return (List<GroupData>) 
+                    new XmlSerializer(typeof(List<GroupData>))
+                   .Deserialize(new StreamReader(@"groups.xml"));
+        }
+
+        public static IEnumerable<GroupData> GroupDataFromJsonFile()
+        {
+            return JsonConvert.DeserializeObject<List<GroupData>>(
+                   File.ReadAllText(@"groups.json"));
+        }
+
+        public static IEnumerable<GroupData> GroupDataFromCsvFile()
         {
             List<GroupData> groups = new List<GroupData>();
             string[] lines = File.ReadAllLines(@"groups.csv");
@@ -45,7 +62,7 @@ namespace WebAddressbookTests
             return groups;
         }
 
-        [Test, TestCaseSource("GroupDataFromFile")]
+        [Test, TestCaseSource("GroupDataFromJsonFile")]
         public void GroupCreationTest(GroupData groups)
         {
 
